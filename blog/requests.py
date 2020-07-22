ddb3af0ffbc9c30d323fcff00a4de173c8455522
peterkush <pkungu004@gmail.com>
2020-07-22 14:53:47 +0300

    added background color

--- blog/requests.py
+++ blog/requests.py
@@ -8,4 +8,4 @@ def get_quote():
     """
     response = requests.get(url).json()
     random_quote = Quote(response.get("author"), response.get("quote"))
-    return random_quote
+    return random_
